58a5cbe9eaf778b424b76f01189c3d90686b1fa3
danielpaul <danielpaul2008@gmail.com>
2015-05-01 13:39:39 +0100

    cleaning up

---
+++ www/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php echo $title; ?> - DeeVeeDee</title>
+        <meta name="description" content="Browse, purchase and download DVDs with a few clicks.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" type="image/png" src="img/icon.png">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <header>
+            <section id="top">
+                <div class="container">
+                    <div id="logo">
+                        <h1>DeeVeeDee</h1>
+                        <h3>Watch DVDs in a few clicks</h3>
+                    </div>
+                    <div id="search">
+                        Search box
+                    </div>
+                </div>
+
+            </div>
+            <section id="menu">
+                <div class="container">
+                    <ul>
+                        <li><a href="#" title="">Home</a></li>
+
+                        <!-- login / register float right -->
+                    </ul>
+                </div>
+            </div>
+        </header>
---
+++ www/index.php
@@ -0,0 +1,35 @@
+<?php
+  // Page information
+  $title = "Homepage";
+?>
+
+
+<?php include('header.php'); ?>
+
+
+        <section id="main">
+          <div class="container">
+
+            <div class="left">
+
+              <!-- Top Movies -->
+
+            </div><!-- / .left -->
+
+            <div class="right">
+
+              <div id="featured">
+                iframe
+                <h2>Movie title</h2>
+              </div><!-- / #featured -->
+
+              <div id="latest">
+
+              </div>
+
+            </div><!-- / .right -->
+
+          </div><!-- / .container -->
+        </section><!-- / #main -->
+
+<?php include('footer.php'); ?>

be32a08d70c7f7a7422b605b39dadd08f0d67578
danielpaul <danielpaul2008@gmail.com>
2015-05-01 11:15:23 +0100

    added search functionality

--- website/results.php
+++ website/results.php
@@ -33,7 +33,7 @@
 
         <div class="img">
           <a class="links" href="movie.php?v=<?php echo $line['movid_id']; ?>">
-            <img src="template/images/<?php echo $line['movid_id']; ?>.jpg" alt="<?php echo $line['movie_title']; ?>" width="130" height="190">
+            <img src="images/<?php echo $line['movid_id']; ?>.jpg" alt="<?php echo $line['movie_title']; ?>" width="130" height="190">
             <div class="desc"><?php echo $line['movie_title']; ?> &euro;<?php echo $line['movie_price']; ?></div>
           </a>
         </div>

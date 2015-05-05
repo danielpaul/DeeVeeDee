3d0a18f8ab050990e7a862fa7c8200e52f7315df
danielpaul <danielpaul2008@gmail.com>
2015-05-05 19:19:14 +0100

    added config sample file

---
+++ app/php/config_sample.php
@@ -0,0 +1,13 @@
+<?php
+
+  /* Rename this file to config.php and update details to correct database */
+
+  /* ---------- Global variables ---------- */
+
+  $db_host = "";
+  $db_name = "";
+  $db_user = "";
+  $db_pass = "";
+
+
+?>

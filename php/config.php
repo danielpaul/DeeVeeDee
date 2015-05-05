9f02d4d106ad90bd73d199d1cc1a44b4b913c1c9
danielpaul <danielpaul2008@gmail.com>
2015-05-06 00:38:46 +0100

    moved some files

---
+++ php/config.php
@@ -0,0 +1,11 @@
+<?php
+
+  /* ---------- Global variables ---------- */
+
+  $db_host = "webcourse.cs.nuim.ie";
+  $db_name = "cs230";
+  $db_user = "cs230teamd6";
+  $db_pass = "Ootheigh";
+
+
+?>
